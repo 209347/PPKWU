8f175f7c78d6c1269ad5cf715922cdd1bf2aa8f4
209347 <209347@edu.p.lodz.pl>
2020-01-14 17:24:48 +0100

    Ex4 Added model, moved model from ex3

---
+++ src/main/java/com/example/demo/model/UserModel.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+public class UserModel {
+
+    private String name;
+
+    private String surname;
+
+    private String place;
+
+    private String title;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
