30d8cf8e3a9e70b69f66d5bf30b567edaa107026
209347 <209347@edu.p.lodz.pl>
2020-01-15 20:30:57 +0100

    Ex4 Controller updated for returning html templates. Added necessary dependencies

--- src/main/java/com/example/demo/controller/VCardController.java
+++ src/main/java/com/example/demo/controller/VCardController.java
@@ -4,27 +4,26 @@ import com.example.demo.model.UserModel;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-@RestController
+@Controller
 public class VCardController {
 
-    @GetMapping("/api/getUsersData/{phrase}") @ResponseStatus(HttpStatus.OK) public ArrayList<UserModel> getUsersData(@PathVariable String phrase) throws IOException {
+    @GetMapping("/") public String main() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/api/getUsersData/{phrase}", method = RequestMethod.GET) @ResponseBody public ArrayList<UserModel> getUsersData(@PathVariable String phrase) throws IOException {
         ArrayList<UserModel> users = new ArrayList<>();
 
-        Document doc = Jsoup.connect(
-                "https://adm.edu.p.lodz.pl/user/users.php?search=" + phrase).get();
+        Document doc = Jsoup.connect("https://adm.edu.p.lodz.pl/user/users.php?search=" + phrase).get();
         Elements all = doc.select("div.user-info");
         Elements names =  all.select("h3");
         Elements titles =  all.select("h4");
-        Elements places = all.select("span.item-content");
 
         for(int i = 0; i < all.size(); i++){
             UserModel u = new UserModel();
